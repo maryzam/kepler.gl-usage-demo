d95d190df3e13f0084ba464ccb4136606e850f41
maryzam <zamo.code@gmail.com>
2018-07-22 17:04:39 +0300

    Imporove loadParkingData action

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { throttle } from 'lodash';
+
+import KeplerGlSchema from 'kepler.gl/schemas';
 import { wrapTo, addDataToMap } from 'kepler.gl/actions';
 
 import { loadParkingData } from './store/actions';
@@ -10,10 +12,16 @@ import ParkingMap from './components/ParkingMap';
 
 import { getMapConfig, MAP_MODE } from './configs/map';
 
+const initialDataset = {
+			       			info: { 
+			       				id: 'parking_data', 
+			       				label: 'Moscow Paid Parking' },
+				          	data: []
+				        };
+
 class App extends React.Component {
 
 	state = {
-		mapMode: MAP_MODE.POINTS,
 		width: window.innerWidth,
     	height: window.innerHeight
 	};
@@ -26,31 +34,38 @@ class App extends React.Component {
 	}
 
 	updateMap = (mode) => {
-		const config = getMapConfig(mode);
+		/*const config = getMapConfig(mode);
+		const datasets = this.getMapDataset();
 		this.props.dispatch(
 			wrapTo('parking_map',
-				addDataToMap({ datasets: [], config })
-			));
+				addDataToMap({ datasets , config })
+			)); */
 	}
 
 	toggleMapMode = ({ target }) => {
-
 		const mapMode = target.dataset.mode;
 		this.updateMap(mapMode);
 		this.setState({ mapMode });
 	}
 
-	shouldComponentUpdate(nextProps, nextState) {
-		console.log(this.props.app.isLoading);
+	getMapDataset() {
+		const { keplerGl } = this.props;
+	    const { parking_map } = keplerGl;
+	    return !parking_map ? 
+	    			initialDataset:
+	    			KeplerGlSchema.getDatasetToSave(parking_map);
+	}
+
+/*	shouldComponentUpdate(nextProps, nextState) {
 		const { mapMode, width, height } = this.state;
 		return (mapMode !== nextState.mapMode) ||
 			Math.abs(width - nextState.width) > 0.5 ||
 			Math.abs(height - nextState.height) > 0.5;
-	}
+	} */
 
 	componentDidMount() {
 		this.props.dispatch(loadParkingData());
-		this.updateMap(this.state.mapMode);
+		//this.updateMap(this.state.mapMode);
 		
 		this.onResize = throttle(this.updateSize, 150, { trailing: true });
 		window.addEventListener('resize', this.onResize);
@@ -61,6 +76,12 @@ class App extends React.Component {
 	}
 
 	render() {
+		console.log("render");
+		const map = this.props.keplerGl.parking_map;
+		if (!!map) {
+			console.log(map.visState)
+		};
+
 		const { width, height, mapMode } = this.state;
 
 		return (
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,43 +1,52 @@
 import Processors from 'kepler.gl/processors';
-import { wrapTo, updateVisData } from 'kepler.gl/actions';
+import KeplerGlSchema from 'kepler.gl/schemas';
+import { wrapTo, addDataToMap } from 'kepler.gl/actions';
+
+import { getMapConfig } from '../configs/map';
 
 export const SET_LOADING_STATUS = 'SET_LOADING_STATUS';
+export const UPDATE_DATASET = 'UPDATE_DATASET';
 
 const demoDataUrl = 'data/parking.csv';
 
 export function loadParkingData() {
 	
-    return (dispatch) => {
+    return (dispatch, getState) => {
     	
-        dispatch({
-            type: SET_LOADING_STATUS,
-            isLoading: true
-        });
+        setLoadingStatus(dispatch,true);
         
         fetch(demoDataUrl)
         	.then(response => response.text())
             .then(data => {
-
-            	dispatch({
-		         	type: SET_LOADING_STATUS,
-            		isLoading: false
-		        });
-
-            	const datasets = [{
-		        	info: { 
-		          		id: 'parking_data',
-		          		label: 'Moscow Paid Parking' 
-		          	},
-		          	data: Processors.processCsvData(data)
-		         }]; 
-
-		         dispatch(
-		         	wrapTo('parking_map',
-						updateVisData({
-							datasets,
-					        config: {} //todo
-						})
-					)); 
+            	setLoadingStatus(dispatch, false);
+            	setLoadedData(dispatch, data, getState());
 		    }); 
     };
+}
+
+function setLoadingStatus(dispatch, isLoading = false) {
+	dispatch({
+        type: SET_LOADING_STATUS,
+        isLoading
+    });
+}
+
+function setLoadedData(dispatch, data, state) {
+	const datasets = [{
+		       			info: { 
+			          		id: 'parking_data',
+			          		label: 'Moscow Paid Parking' 
+			          	},
+			          	data: Processors.processCsvData(data)
+			          }]; 
+	
+	const config = getMapConfig(state.app.mapMode);
+	
+	dispatch(
+	   	wrapTo('parking_map',
+			addDataToMap({
+				datasets,
+				config
+			})
+		)); 
 }
